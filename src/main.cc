484448fe4d9a1e6955c87860abfb7384c970cca8
Adrián González Galván <alu0101321219@ull.edu.es>
2022-12-02 14:03:31 +0000

    Lectura del json hecha

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <fstream>
+#include "../include/json.h"
 #include "../include/document.h"
+
+using json = nlohmann::json;
 
 // Cuenta el número de términos de todo el corpus de documentos
 unsigned
@@ -32,6 +35,63 @@ getIDF(Term term, std::vector<Document> documents) {
 }
 
 int main(int argc, char* argv[]) {
+    
+  // Comprobamos que se haya ejecutado de la forma correcta
+  if (!argv[1]) {
+    std::cout << "Modo de empleo: ";
+    std::cout << "./main [documents.txt] [stop_words.txt] [lexeme.json]\n" ;
+    std::cout << "Pruebe './main --help' para más información.\n";   
+    return 1;
+  }
+
+  // En caso de usar como parámetro "--help"
+  std::string help = "--help";
+  if(argv[1] == help) {
+    std::ifstream help_txt;
+    help_txt.open("help.txt");
+    if (!help_txt.fail()) {
+      while (!help_txt.eof()) {
+        std::getline(help_txt, help);
+        std::cout << help << "\n";
+      }
+    }
+    help_txt.close();
+    return 1;
+  }
+
+  // Archivo de lectura de documentos
+  std::ifstream inputDocuments;
+  inputDocuments.open(argv[1]);
+
+  if (inputDocuments.fail()) {
+    std::cout << "No se pudo abrir archivo de lectura de documentos, ";
+    std::cout << "comprueba si se ingresó un nombre correcto\n";
+    return 1;
+  }
+
+  // Archivo de lectura de stop-words
+  std::ifstream inputStopWords;
+  inputStopWords.open(argv[2]);
+
+  if (inputStopWords.fail()) {
+    std::cout << "No se pudo abrir archivo de lectura, ";
+    std::cout << "comprueba si se ingresó un nombre correcto\n";
+    inputDocuments.close();
+    return 1;
+  }
+
+  // Archivo de lectura lexematizacion
+  std::ifstream inputLexeme(argv[3]);
+
+  if (inputLexeme.fail()) {
+    std::cout << "No se pudo abrir archivo de lexematización, ";
+    std::cout << "comprueba si se ingresó un nombre correcto\n";
+    inputDocuments.close();
+    inputStopWords.close();
+    return 1;
+  }
+
+  json lexemeData = json::parse(inputLexeme);
 
   // Descripción del menú principal (idea)
   /*
@@ -61,6 +121,11 @@ int main(int argc, char* argv[]) {
 
   std::cout << d1 << "\n";
   std::cout << "VECTOR SIZE == " << d1.getLength() << "\n";
+
+  inputDocuments.close();
+  inputStopWords.close();
+  inputLexeme.close();
+
   return 0;
 }
 
